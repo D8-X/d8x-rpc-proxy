2c9462351830f3296cf665dc8a8159658cd8297f
souhailaS <serbout.souhaila@gmail.com>
2026-04-23 10:39:28 +0200

    status codes

---
+++ internal/proxy/jsonrpc_errors.go
@@ -0,0 +1,11 @@
+package proxy
+
+const (
+	JsonRpcErrInvalidRequest = -32600 
+	JsonRpcErrMethodNotFound = -32601 
+	JsonRpcErrInternal       = -32603
+	JsonRpcErrServerGeneric  = -32000 
+	JsonRpcErrUnauthorized   = -32001 
+	JsonRpcErrMethodBlocked  = -32004 
+	JsonRpcErrRateLimit      = -32005 
+)
